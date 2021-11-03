3fcdc39702d7b85916821f7034a8af51444f8304
yanagiya-umi <yana3umi@gmail.com>
2021-11-03 12:15:22 +0900

    [add]devise_新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'posts/index'
-  get 'users/show'
   devise_for :users
   root "homes#top"
   get '/about' => "homes#about"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
 end
