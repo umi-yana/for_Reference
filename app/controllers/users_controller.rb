673159be9595f51d8e45c93c78f01a1417ebed5c
yanagiya-umi <yana3umi@gmail.com>
2021-11-05 15:03:57 +0900

    [add]user-show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.all.order(id: "DESC") 
   end
 
   def index
     @post = Post.all
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+  
 end
