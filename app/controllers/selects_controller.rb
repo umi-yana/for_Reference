9329ccba621ece3a7d69d4b12e6f0079547b2ea9
yanagiya-umi <yana3umi@gmail.com>
2021-11-09 11:12:04 +0900

    [fix]devise

--- app/controllers/selects_controller.rb
+++
@@ -1,2 +0,0 @@
-class SelectsController < ApplicationController
-end
--- test/controllers/selects_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SelectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
