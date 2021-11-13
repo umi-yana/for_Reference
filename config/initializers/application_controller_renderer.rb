0245cd554d8a96e5f9b1d24dd20e3c79657d1219
yanagiya-umi <yana3umi@gmail.com>
2021-11-13 04:35:31 +0000

    fix

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c1209d35f35eb649f9a831ca45cf9d04172312b28bfe4d617c67ec8e4c5ad2658ebf6804bb9b5a22642fcc51193624591133fcd95eba2c43f694b98e86ce6a68'
