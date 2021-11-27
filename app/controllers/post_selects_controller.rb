df1e281ce4d63121e47434dd8790ec4741230a09
yanagiya-umi <yana3umi@gmail.com>
2021-11-27 17:38:48 +0900

    likes_edit

--- app/controllers/post_selects_controller.rb
+++ app/controllers/post_selects_controller.rb
@@ -25,8 +25,9 @@ class PostSelectsController < ApplicationController
 
   def show_a
     @post = Post.find(params[:post_id])
-    @post_selects = PostSelect.where(post_id: params[:post_id])
-    select_alls = PostSelect.all.size 
+    @post_selects = PostSelect.where(post_id: params[:post_id]).order(creat_at: "DESC")
+    
+    
   end
 
   def show_b
