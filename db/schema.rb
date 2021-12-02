68fc2d27fa8538225163abf3991ab670e67a0d3f
yanagiya-umi <yana3umi@gmail.com>
2021-12-02 13:19:37 +0000

    [fix]comment

--- db/schema.rb
+++ db/schema.rb
@@ -46,12 +46,12 @@ ActiveRecord::Schema.define(version: 2021_12_01_133011) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.integer "post_tag_id"
     t.integer "user_id"
     t.text "post_body"
     t.text "select_a"
     t.text "select_b"
     t.string "post_image_id"
-    t.integer "post_tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "score", precision: 5, scale: 3
