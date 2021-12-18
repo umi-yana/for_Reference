8b0ac8da101bf96e914c7927bee0d81cbca5c5e2
yanagiya-umi <yana3umi@gmail.com>
2021-12-18 18:35:32 +0900

    [fix]post_index(n+1)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   def index
     @post = Post.new
-    @posts = Post.includes(:user, :comments, :post_selects).order(id: "DESC").page(params[:page]).per(20)
+    @posts = Post.includes(:user, :comments, :post_selects, :tags).order(id: "DESC").page(params[:page]).per(20)
     @tag_list = Tag.includes(:post).all.order(id: "DESC")
   end
 
