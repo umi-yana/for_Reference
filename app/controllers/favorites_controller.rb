98847bf4b9481943b409eb6632a148090e5b4f12
yanagiya-umi(imac) <yana3umi@gmail.com>
2022-01-07 23:36:17 +0900

    [fix]commentout

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,12 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
     favorite.destroy
-    # redirect_back(fallback_location: root_path)
   end
 
   def show
